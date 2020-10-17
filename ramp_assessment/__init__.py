a2d0b4c8b1c9eeabe4c66324153b5299aab3472c
Matthew Williams <matthew.williams@impact.com>
2020-10-17 11:03:10 +0200

    Add endpoint to post comma separated string

--- ramp_assessment/__init__.py
+++ ramp_assessment/__init__.py
@@ -1,9 +1,14 @@
 __version__ = '0.1.0'
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+@app.route('/multiply', methods=["POST"])
+def multiply():
+    return jsonify(request.get_json(force=True))
