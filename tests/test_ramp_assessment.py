a64ca782f1534d97fae2ff3f3da88df1303e467c
Matthew Williams <matthew.williams@impact.com>
2020-10-17 11:21:24 +0200

    Add unit test for multiply endpoint

--- tests/test_ramp_assessment.py
+++ tests/test_ramp_assessment.py
@@ -6,3 +6,8 @@ client = app.test_client()
 def test_index():
     response = client.get('/')
     assert b"Hello, World!" in response.data
+
+def test_multiply():
+    input = "0.1,0.9,0.123,0.99,0.5,1.0,0"
+    response = client.post('/multiply', data='{{"input": "{}"}}'.format(input))
+    assert b"0.1,0.9,0.123,0.99,0.5,1.0,0" in response.data
