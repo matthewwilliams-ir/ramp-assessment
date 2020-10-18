67ea4e3efa676ef4176dac320feff871cf1099cb
Matthew Williams <matthew.williams@impact.com>
2020-10-18 16:01:02 +0200

    Separate models from service

---
+++ ramp_assessment/multiply/models.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+original_results_df = pd.DataFrame(
+    {"Week 1": [1,1,1,1,1,1,1],"Week 2": [2,2,2,2,2,2,2],"Week 3": [3,3,3,3,3,3,3]}, 
+    index=[1,2,3,4,5,6,7],
+    dtype="float64", 
+    copy=True
+)
+
+class InputVector:
+
+    def __init__(self, input_str):
+        # TODO: Validate input
+        self.input_list = input_str.split(",")
+
+    def to_numpy_float_array(self):
+        float_array = list(map(float, self.input_list))
+        return np.array(float_array)
+
--- ramp_assessment/multiply/service.py
+++ ramp_assessment/multiply/service.py
@@ -1,18 +1,8 @@
-import numpy as np
-import pandas as pd
+from .models import InputVector, original_results_df
 
 def transform(input_str):
-
-    # TODO: Validate input here
-    # TODO: Map input to numpy float array
-    input_list = input_str.split(",")
-    input_float_array = list(map(float, input_list))
-    np_array = np.array(input_float_array)
-
-    # TODO: Create 3*7 DataFrame
-    df = pd.DataFrame({"A": [1,1,1,1,1,1,1],"B": [2,2,2,2,2,2,2],"C": [3,3,3,3,3,3,3]}, index=[1,2,3,4,5,6,7], dtype="float64", copy=True)
-
-    # TODO: Transform
-    result_df = df.mul(np_array, axis=0) # Compare with .apply()
+    input_vector = InputVector(input_str)
+    numpy_float_array = input_vector.to_numpy_float_array()
+    result_df = original_results_df.mul(numpy_float_array, axis=0) # Compare with .apply()
 
     return result_df.to_json()
